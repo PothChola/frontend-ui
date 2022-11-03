14924ebcc2469b1de9aadb7d45a7dc86844a7106
Shreya Jash <shreyajash98@gmail.com>
2022-11-04 01:50:59 +0530

    feature_update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import "../styles/footer.css";
 
 function Footer() {
   return (  
-    <footer className="footer p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="#" class="hover:underline">PothChola</a>. All Rights Reserved.
+    <footer className="footer bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+    <span className="text-sm ml-7 text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="#" class="hover:underline">PothChola</a>. All Rights Reserved.
     </span>
-    <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+    <ul className="footul flex items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
             <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
         </li>
