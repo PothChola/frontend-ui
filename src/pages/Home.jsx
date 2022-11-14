e66ba8258d6d63dc5a052e3612e2208da3ed38c8
Shreya Jash <shreyajash98@gmail.com>
2022-11-14 09:59:05 +0530

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "../styles/home.css"
 import buses from "../assets/buses.svg";
+import preference from "../assets/preference.svg";
+
 export default function Home() {
   return (
     <div className='homePage'>
-      <h1 className="font-bold italic pb-5 text-blue-600">Welcome!</h1>
+      <h1 className="welcome font-bold italic pb-5 text-blue-600">Welcome!</h1>
       <div className='source pb-3 text-[16px]'>Enter Souce -
         <input className='inputSource pl-2 bg-gray-300 rounded-full h-8' type="text"/>
       </div>
@@ -12,7 +14,13 @@ export default function Home() {
         Enter Destination -
           <input className='inputDestination pl-2 bg-gray-300 h-8 rounded-full' type="text"/>
       </div>
-      <button className='searchButton w-32'>Search</button>
+      <button className='searchButton w-32' >Search</button>
+      <h4>Choose your means of Travel- </h4>
+      <img src={preference} alt="preference"/>
+      <h3 className='or'>OR</h3>
+      <div className='buttonsize'>
+      <button className='oroption'>Cheapest and shortest route.</button>
+        </div>
       <h4>Available buses in this route-</h4>
       <img src={buses} alt="buses avialabe"/>
     </div>
